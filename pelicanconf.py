517131b7f568a006af936094e77fa4d1bdba3a2a
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-11-18 22:15:47 +0100

    Added stwitter account ;).

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,14 +23,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('github', 'https://github.com/pauschaleaussagen'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/PauschalAussage'),
+          ('github' , 'http://github.com/pauschaleaussagen'),)
 
 DEFAULT_PAGINATION = False
 
