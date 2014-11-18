c3b8a3fb1dc66f149725a7e120da8b397ee39b47
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-11-18 19:00:13 +0100

    Testing a theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'de'
 
+THEME="theme/pelican-bootstrap3"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
